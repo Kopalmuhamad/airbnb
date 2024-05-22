bbb589c89e1576b9958c1e8ba73a8ccdbdfb45e5
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-05-22 07:32:42 +0700

    issues: ubah card slider pada propery list menjadi list aja

---
+++ src/app/home/page.tsx
@@ -0,0 +1,17 @@
+import PropertyList from "@/app/home/PropertyList";
+import Banner from "@/app/home/Banner";
+import SearchForm from "./SearchForm";
+
+const Home = () => {
+  return (
+    <section className="w-full h-full text-black">
+      <Banner />
+      <div className="container h-full mx-auto flex items-center justify-center flex-col text-center gap-y-8">
+        <SearchForm />
+        <PropertyList value="Recent House" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
