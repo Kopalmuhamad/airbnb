f199b0e794fe4295df69b97c3f19799bba805c6f
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-05-22 10:02:44 +0700

    issues:menyesuaikan font size dari label dengan kebutuhan

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -7,8 +7,16 @@ interface LabelProps {
   bold?: boolean;
 }
 
-const Label = ({ children, light, normal=true, bold }: LabelProps) => {
-  return <label className={`text-base ${light && "font-normal"} ${normal && "font-semibold"} ${bold && "font-bold"} sm:text-lg`}>{children}</label>;
+const Label = ({ children, light, normal = true, bold }: LabelProps) => {
+  return (
+    <label
+      className={`text-base ${light && "font-normal"} ${
+        normal && "font-semibold"
+      } ${bold && "font-bold"} sm:text-lg`}
+    >
+      {children}
+    </label>
+  );
 };
 
 export default Label;
