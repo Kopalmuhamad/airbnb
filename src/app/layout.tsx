ad204be1e4f7f9aa8b973e2efcb6848854035af9
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-05-22 16:14:20 +0700

    issues:fixed font family error quicksand not found

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const inter = Quicksand({ subsets: ["latin"] });
+const inter = Quicksand({ subsets: ["latin"], display: 'swap', adjustFontFallback: false});
 
 export const metadata: Metadata = {
   title: "Create Next App",
